471b6440d45e9b3106c88803f7c10090728efec6
yyyyasu <yyyyasu7@gmail.com>
2020-03-21 18:17:22 +0900

    fix: vue.config.js
    ビルドファイルの出力先をdocに変更

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  publicPath: '/mogamiya_vue',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false,
+
 }
